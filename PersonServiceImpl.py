6e40feebef4f45654c94245014525e2398306419
liudongguang <liudongguang@qq.com>
2017-06-17 16:27:46 +0800

    Initial commit

---
+++ PersonServiceImpl.py
@@ -0,0 +1,19 @@
+# _*_ coding:utf-8 _*_
+__auther__='刘东光'
+
+from py.thrift.generated import ttypes
+
+class PersonServiceImpl:
+    def getPersonByUsername(self,username):
+        print "got client parm:"+username
+        person=ttypes.Persion()
+        person.username=username
+        person.age=20
+        person.married=False
+        return person
+
+    def savePerson(self,person):
+        print 'got client param:'
+        print person.username
+        print person.age
+        print person.married
---
+++ py_client.py
@@ -0,0 +1,44 @@
+# _*_ coding:utf-8 _*_
+__auther__='刘东光'
+
+from py.thrift.generated import PersionService
+from py.thrift.generated import  ttypes
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TCompactProtocol
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+try:
+    tSocket=TSocket.TSocket('localhost',8899)
+    tSocket.setTimeout(600)
+
+    transport=TTransport.TFramedTransport(tSocket)
+    protocol=TCompactProtocol.TCompactProtocol(transport)
+    client=PersionService.Client(protocol)
+
+    transport.open()
+
+    person=client.getPersionByUsername("张三")
+
+    print person.username
+    print person.age
+    print person.married
+
+    print '---------'
+
+    newPerson=ttypes.Persion()
+    newPerson.username='李四'
+    newPerson.age=63
+    newPerson.married=True
+
+    client.savePersion(newPerson)
+
+    transport.close()
+
+except Thrift.TException,tx:
+    print '%s' % tx.message
